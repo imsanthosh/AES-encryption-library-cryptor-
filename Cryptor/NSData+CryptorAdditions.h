db845dcffa188997de6a54c4fa2b44bc6c2986ae
Santhosh K <Santhosh.K@hotstar.com>
2017-04-23 18:23:40 +0530

    first commit

---
+++ Cryptor/NSData+CryptorAdditions.h
@@ -0,0 +1,13 @@
+//
+//  NSData+CryptorAdditions.h
+//  Cryptor
+//
+//  Created by K, Santhosh on 23/04/17.
+//  Copyright © 2017 K, Santhosh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (CryptorAdditions)
+
+@end
---
+++ Cryptor/NSData+CryptorAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSData+CryptorAdditions.m
+//  Cryptor
+//
+//  Created by K, Santhosh on 23/04/17.
+//  Copyright © 2017 K, Santhosh. All rights reserved.
+//
+
+#import "NSData+CryptorAdditions.h"
+
+@implementation NSData (CryptorAdditions)
+
+@end
